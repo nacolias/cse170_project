7493ad647e5d02cacf36d714e9a94ce3220ab4ac
David Canas <dcanas@ucsd.edu>
2015-01-29 19:48:43 -0800

    Setting up...

--- index.php
+++ index.php
@@ -3,12 +3,83 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Foundation | Welcome</title>
+    <title>Time Space</title>
     <link rel="stylesheet" href="css/foundation.css" />
     <script src="js/vendor/modernizr.js"></script>
   </head>
   <body>
     
+      <!--Nav-->
+      <div class="off-canvas-wrap" data-offcanvas> 
+          <div class="inner-wrap"> 
+              <nav class="tab-bar"> 
+                  <section class="left-small"> 
+                      <a class="left-off-canvas-toggle menu-icon" href="#">
+                          <span>
+                              
+                          </span>
+                      </a> 
+                  </section> 
+                  <section class="middle tab-bar-section"> 
+                      <h1 class="title">Time Space</h1> 
+                  </section> 
+                  <section class="right-small"> 
+                      <a class="right-off-canvas-toggle menu-icon" href="#">
+                          <span>
+                          </span>
+                      </a> 
+                  </section> 
+              </nav> 
+              <aside class="left-off-canvas-menu"> 
+                  <ul class="off-canvas-list"> 
+                      <li>
+                          <label>Network</label>
+                      </li> 
+                      <li>
+                          <a href="#">All UCSD</a>
+                      </li> 
+                      <li>
+                          <a href="#">Just Friends</a>
+                      </li> 
+                  </ul> 
+              </aside> 
+              
+              <aside class="right-off-canvas-menu"> 
+                  <ul class="off-canvas-list"> 
+                      <li>
+                          <label>Settings</label>
+                      </li> 
+                      <li>
+                          <a href="#">
+                            Availabilities 
+                          </a>
+                      </li> 
+                      <li>
+                          <a href="#">
+                            Pause    
+                          </a>
+                      </li> 
+                  </ul> 
+              </aside> 
+              <section class="main-section"> 
+                    <div class="person">
+                        <div class="row">
+
+                        </div> 
+
+                        <div class="row">
+                            
+                        </div>
+                        
+                        <div class="row">
+                        
+                        </div>
+                    </div>
+              </section> 
+              <a class="exit-off-canvas">
+              </a> 
+          </div> 
+      </div>
     
     <script src="js/vendor/jquery.js"></script>
     <script src="js/foundation.min.js"></script>
