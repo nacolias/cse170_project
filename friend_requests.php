0cf402a9ef3765dd514a951f692b6a5186fbb942
David Canas <davidxcanas@gmail.com>
2015-02-05 19:27:27 -0800

    Finished a potential settings page

--- friend_requests.php
+++ friend_requests.php
@@ -19,10 +19,10 @@
 		    		<div class="small-centered">
 				    	<center>
 				    		<i class="fa fa-users fa-2x"></i>
-							<i class="fa fa-search fa-2x"></i>
-							<a href="index.php"><i class="fa fa-home fa-2x"></i></a>
-							<i class="fa fa-user-plus fa-2x"></i>
-							<i class="fa fa-gear fa-2x"></i>
+                            <i class="fa fa-search fa-2x"></i>
+                            <a href="index.php"><i class="fa fa-home fa-2x"></i></a>
+                            <a href="friend_requests.php"><i class="fa fa-user-plus fa-2x"></i></a>
+                            <a href="settings.html"><i class="fa fa-gear fa-2x"></i></a>
 						</center>
 					</div>
 				</div>
