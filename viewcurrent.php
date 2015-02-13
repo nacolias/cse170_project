5411246aa6369108c950dc555083480200c33309
David Canas <davidxcanas@gmail.com>
2015-02-13 02:38:31 -0800

    It's here nick!

---
+++ viewcurrent.php
@@ -0,0 +1,44 @@
+
+<div class="small-centered small-12 columns">
+	<table>
+		<thead>
+			<tr>
+				<th>Sunday</th>
+				<th>Monday</th>
+				<th>Tuesday</th>
+				<th>Wednesday</th>
+				<th>Thursday</th>
+				<th>Friday</th>
+				<th>Saturday</th>
+			</tr>
+		</thead>
+
+		<tbody>
+			<tr>
+				<td>8:00am - 9:00am</td>
+				<td>12:00pm - 4:30pm</td>
+				<td>7:00pm - 9:00pm</td>
+			</tr>
+			<tr>
+				<td>8:00am - 9:00am</td>
+				<td>12:00pm - 4:30pm</td>
+				<td>7:00pm - 9:00pm</td>
+			</tr>
+			<tr>
+				<td>8:00am - 9:00am</td>
+				<td>12:00pm - 4:30pm</td>
+				<td>7:00pm - 9:00pm</td>
+			</tr>
+			<tr>
+				<td>8:00am - 9:00am</td>
+				<td>12:00pm - 4:30pm</td>
+				<td>7:00pm - 9:00pm</td>
+			</tr>
+			<tr>
+				<td>8:00am - 9:00am</td>
+				<td>12:00pm - 4:30pm</td>
+				<td>7:00pm - 9:00pm</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
