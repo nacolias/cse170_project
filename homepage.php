aa41127e8e4301755f67724c24bb7e24af6a2d26
David Canas <davidxcanas@gmail.com>
2015-02-12 23:46:32 -0800

    added email button

--- homepage.php
+++ homepage.php
@@ -38,12 +38,6 @@ else
                 <img src="img/guy2.jpg" />
             </div>
         </div>
-
-        <div class="row">
-            <div class="small-centered small-8 columns">
-                <center><h4>Flake Rate: 20%</h4></center>
-            </div>
-        </div>
         
         <div class="row">
             <div class="small-centered small-8 columns">
@@ -55,6 +49,15 @@ else
             <div class="small-centered small-8 columns buttonarea">
                 <center>
                     <a href="sms:1-818-632-2449" class="small button">SMS</a>
+                </center>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="small-centered small-8 columns buttonarea">
+                <center>
+                    <a href="sms:1-818-632-2449" class="small button">SMS</a>
+                    <a href="mailto:dcanas@ucsd.edu" class="small right button">Email</a>
                 </center>
             </div>
         </div>
